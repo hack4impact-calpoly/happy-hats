6af01d35edb1ed076244d1bab65d82f7cdf5c84f
KennaReed <mreed16@calpoly.edu>
2021-01-16 11:56:06 -0800

    Base Annoucement

--- frontend/src/components/Announcement.js
+++ frontend/src/components/Announcement.js
@@ -1,10 +1,55 @@
+import React, {useState} from 'react'
 
+function Announcement(props) {
+    const [annoucment, setAnnoucement] = useState(
+        {
+            title: '',
+            content: '',
+            id:''
+        }
+    );
 
+    function handleChange(event) {
+        const {box, value} = event.target;
+        if (box === "title") {
+            setAnnoucement(
+                {title: annoucment['title'], content: value}
+            );
+        } else {
+            setAnnoucement(
+                {title: value, content: annoucment['content']}
+            );
+        }
+    }
 
-function AnnouncementBlock (){
+    function submitForm() {
+        props.handleSubmit(annoucment);
+        setAnnoucement({title: '', content: '', id:''});
+    }
+
     return(
-        <p>Announcement Title</p>
-    )
+        <div>
+            <h1>Make An Accouncement</h1>
+            <form>
+                <label htmlFor="title">Annoucement Title</label>
+                <input 
+                    type="text"
+                    name="title"
+                    id="title"
+                    value={annoucment.title}
+                    onChange={handleChange} />
+                <label htmlFor='content'>Annoucement Description</label>
+                <input
+                    type="text"
+                    name="content"
+                    id="content"
+                    value={annoucment.content}
+                    onChange={handleChange} />
+                <input type="button" value="Submit Annoucement" onClick={submitForm} />
+            </form>
+        </div>
+    );
 }
 
-export default AnnouncementBlock;
+export default Announcement
+
