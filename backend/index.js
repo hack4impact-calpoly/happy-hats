0747068ffa058178c7c6813286d080efefa678c8
Divya Satrawada <dsatrawada@gmail.com>
2021-01-20 12:08:33 -0800

    Add passport.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const mongoose = require('mongoose')
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+require('dotenv').config();
+
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.dbUser_URL, {
     useNewUrlParser: true,
@@ -20,7 +36,10 @@ const userSchema = new mongoose.Schema ({
     secret: String
 });
 
-const User = new mongoose.model("User", userSchema);
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model('User', userSchema);
 
 app.use(bodyParser.json())
 
