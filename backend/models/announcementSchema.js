5414b8442c0140c883d93f449eebc26ef47b54d9
plandsman11 <parker.rey@gmail.com>
2021-01-20 21:34:48 -0800

    Fixed schema typo. Oopsy

--- backend/models/announcementSchema.js
+++ backend/models/announcementSchema.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 
+//change date to different data type if we find a better one
 const announcementSchema = new mongoose.Schema({
   title: String,
   content: String,
   id: String,
   author: String,
-  data: String
+  date: String 
 }, {collection: "announcements"})
 
 const Announcement = mongoose.model('announcements', announcementSchema)
