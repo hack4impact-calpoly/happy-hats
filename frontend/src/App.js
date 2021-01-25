f15db2c952de2c315d0d7d1e56568d481adba3f0
plandsman11 <parker.rey@gmail.com>
2021-01-25 15:54:00 -0800

    Created basic announcement block to fill in with info from db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Anouncement from './components/Announcement.js'
+import AnouncementBlock from './components/AnnouncementBlock.js'
 
 function App() {
   return (
     <div>
       <Anouncement/>
+      <AnouncementBlock/>
     </div>
     
   );
