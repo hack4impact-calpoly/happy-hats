b1f2bece488baf6e3b3293e56857f8516ba5e646
KennaReed <mreed16@calpoly.edu>
2021-01-25 13:41:13 -0800

    Found better Date Type

--- backend/models/announcementSchema.js
+++ backend/models/announcementSchema.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 
-//change date to different data type if we find a better one
 const announcementSchema = new mongoose.Schema({
   title: String,
   content: String,
   author: String,
-  date: String 
+  date: Date 
 }, {collection: "announcements"})
 
 const Announcement = mongoose.model('announcements', announcementSchema)
