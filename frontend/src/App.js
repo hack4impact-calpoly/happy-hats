497754264ee3ed5837f5e16c16964e13368a6330
Shad Hussain <shad12@outlook.com>
2021-01-25 18:23:48 -0800

    hopefully this works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+ 
+import Login from './Login';
+//import Dashboard from './Dashboard';
+//import Home from './Home';
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink activeClassName="active" to="/login">Login</NavLink><small>(Access without token only)</small>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
-
+ 
 export default App;
