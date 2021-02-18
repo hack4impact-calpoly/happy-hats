825fd67e309277866090b0fa5f57f31f004dc418
plandsman11 <parker.rey@gmail.com>
2021-02-17 17:09:27 -0800

    Modified index.js to combine endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const dotenv = require('dotenv');
+const dotenv = require('./.env');
 const MongooseConnector = require('./db-helper');
 const app = express()
 
@@ -41,27 +41,46 @@ app.use((req, res, next) => {
 })
 
 app.get('/api/announcement', async (req, res) => {
-  a = await announcement.find({})
-  res.status(200).json(a)
+  const date = req.query.date
+  const author = req.query.author
+  const title = req.query.title
+
+  if(date !== undefined){
+    let announcements = await announcement.find({date: date})
+    res.status(200).json(announcements)
+  }
+  else if(author !== undefined){
+    let announcements = await announcement.find({author: author})
+    res.status(200).json(announcements)
+  }
+  else if(title !== undefined){
+    let announcements = await announcement.find({title: title})
+    res.status(200).json(announcements)
+  }
+  else{
+    announcements = await announcement.find({})
+    res.status(200).json(announcements)
+  }
+  
 })
 
-app.get('/api/announcement/t/:title', async (req, res) => {
-  const title = req.params.title
-  let a = await announcement.find({title: title})
-  res.status(200).json(a)
-})
+// app.get('/api/announcement/t/:title', async (req, res) => {
+//   const title = req.params.title
+//   let a = await announcement.find({title: title})
+//   res.status(200).json(a)
+// })
 
-app.get('/api/announcement/a/:author', async (req, res) => {
-  const author = req.params.author
-  let a = await announcement.find({author: author})
-  res.status(200).json(a)
-})
+// app.get('/api/announcement/a/:author', async (req, res) => {
+//   const author = req.params.author
+//   let a = await announcement.find({author: author})
+//   res.status(200).json(a)
+// })
 
-app.get('/api/announcement/d/:date', async (req, res) => {
-  const date = req.params.date
-  let a = await announcement.find({date: date})
-  res.status(200).json(a)
-})
+// app.get('/api/announcement/d/:date', async (req, res) => {
+//   const date = req.params.date
+//   let a = await announcement.find({date: date})
+//   res.status(200).json(a)
+// })
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
