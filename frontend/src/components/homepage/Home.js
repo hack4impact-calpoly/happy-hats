95cef06b79fafb4c995321dafeb878d9c388175e
Divya Satrawada <dsatrawada@gmail.com>
2021-03-25 19:01:56 -0700

    Volunteer and Hospital Homepage

--- frontend/src/components/homepage/Home.js
+++ frontend/src/components/homepage/Home.js
@@ -13,35 +13,62 @@ export default function Home(props){
                         <Link to="/volunteers" className='link' >
                             <button className="buttonFormat volunteer">Manage Volunteers</button>
                         </Link>
-    
+
                         <Link to="/announcements" className='link' >
                             <button className="buttonFormat announcement">Make an Announcement</button>
                         </Link>
                     </div>
-    
+
                     <div style={{display: 'flex', justifyContent: 'space-around'}}>
                         <Link to="/calendar" className='link' >
                             <button className="buttonFormat calendar">Manage Calendar</button>
                         </Link>
                     </div>
-    
+
                 </div>
             </div>)
-    
+
         }else if(props.role === "hospital"){
-            setPageContent(<h1>Hospital</h1>)
+            setPageContent(<div style={{display: 'flex', width: '66%', justifyContent: 'center'}}>
+                <div style={{display: 'flex', justifyContent: 'space-between', width: '60%', height: '180px', flexDirection: 'column', marginTop: '80px'}}>
+                    <div style={{display: 'inline-flex', justifyContent: 'space-between', width: '100%'}}>
+                        <Link to="/orders" className='link' >
+                            <button className="buttonFormat order">Manage Cape Orders</button>
+                        </Link>
+
+                        <Link to="/announcements" className='link' >
+                            <button className="buttonFormat announcement">View Announcements</button>
+                        </Link>
+                    </div>
+
+                </div>
+            </div>)
+
         }else{
-            setPageContent(<h1>User</h1>)
+            setPageContent(<div style={{display: 'flex', width: '66%', justifyContent: 'center'}}>
+                <div style={{display: 'flex', justifyContent: 'space-between', width: '60%', height: '180px', flexDirection: 'column', marginTop: '80px'}}>
+                    <div style={{display: 'inline-flex', justifyContent: 'space-between', width: '100%'}}>
+                        <Link to="/calendar" className='link' >
+                            <button className="buttonFormat calendar">Manage Shifts</button>
+                        </Link>
+
+                        <Link to="/announcements" className='link' >
+                            <button className="buttonFormat announcement">View Announcements</button>
+                        </Link>
+                    </div>
+
+                </div>
+            </div>)
         }
     })
 
     return (
         <div className="Home" style={{paddingLeft: '8%', paddingTop: '90px'}}>
-            
+
             <h1 className='welcomeMsg'>Welcome, {props.name}</h1>
-            
+
             {pageContent}
-            
+
         </div>
     )
 }
