25fc674a065dc97d1a7df069a48e0f8f282b202b
Divya Satrawada <dsatrawada@gmail.com>
2021-05-24 15:35:11 -0700

    Merge EventDialog and EventDialogAdmin

--- frontend/src/components/calendar/event-dialog/EventDialog.js
+++ frontend/src/components/calendar/event-dialog/EventDialog.js
@@ -12,18 +12,88 @@ import {
 import { makeStyles } from '@material-ui/core/styles';
 import { Button as BootstrapButton } from "react-bootstrap";
 import CloseIcon from '@material-ui/icons/Close';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import { getAMPMTimeRange, getDayMonthDateStr,getMilitaryTimeFromDate } from '../../../utility/date-time';
 import { storeContext } from '../../../store/Store';
 import React, { useContext, useState } from 'react';
 import CreateEvent, { CustomBasicFormControl } from './create-event/CreateEvent';
 import { Formik, Form as FormikForm } from 'formik';
 import * as Yup from 'yup';
+import { USER_TYPES } from '../../../store/user/User';
 
 const useStyles = makeStyles({
   paper: {
     width: '40%',
   },
 });
+
+function PendingVolunteerInfo(props) {
+  const { volunteer } = props;
+  const { volunteers } = props;
+  const { setVolunteers } = props;
+
+  return (
+    <li>
+      {volunteer.name}: <span className="volunteer-time">{getAMPMTimeRange(volunteer.start, volunteer.end)}</span>
+      <IconButton style={{ padding: 5 }} aria-label="approve" onClick={() =>
+          handleApprove(volunteer, volunteers, setVolunteers)}>
+        <ThumbUpIcon />
+      </IconButton>
+      <IconButton style={{ padding: 0 }} aria-label="reject" onClick={() =>
+          handleReject(volunteer, volunteers, setVolunteers)}>
+        <ThumbDownIcon />
+      </IconButton>
+    </li>
+  );
+}
+
+function ApprovedVolunteerInfo(props) {
+    const { volunteer } = props;
+    const { volunteers } = props;
+    const { setVolunteers } = props;
+
+  return (
+    <li>
+      {volunteer.name}: <span className="volunteer-time">{getAMPMTimeRange(volunteer.start, volunteer.end)}</span>
+      <IconButton style={{ padding: 5 }} aria-label="reject" onClick={() =>
+          handleReject(volunteer, volunteers, setVolunteers)}>
+        <ThumbDownIcon />
+      </IconButton>
+    </li>
+  );
+}
+
+function RejectedVolunteerInfo(props) {
+    const { volunteer } = props;
+    const { volunteers } = props;
+    const { setVolunteers } = props;
+
+  return (
+    <li>
+      {volunteer.name}: <span className="volunteer-time">{getAMPMTimeRange(volunteer.start, volunteer.end)}</span>
+      <IconButton style={{ padding: 5 }} aria-label="approve" onClick={() =>
+          handleApprove(volunteer, volunteers, setVolunteers)}>
+        <ThumbUpIcon />
+      </IconButton>
+    </li>
+  );
+}
+
+
+const handleApprove = (volunteer, volunteers, setVolunteers) => {
+    volunteer.decisionMade = true;
+    volunteer.approved = true;
+    let newVolunteers = [...volunteers];
+    setVolunteers(newVolunteers);
+};
+
+const handleReject = (volunteer, volunteers, setVolunteers) => {
+    volunteer.decisionMade = true;
+    volunteer.approved = false;
+    let newVolunteers = [...volunteers];
+    setVolunteers(newVolunteers);
+};
 
 function VolunteerInfo(props) {
   const { volunteer } = props;
@@ -124,6 +194,20 @@ function EventDialog(props) {
   const classes = useStyles();
   const [{ user }, ] = useContext(storeContext);
 
+  const [volunteers, setVolunteers] = useState(event?.volunteers);
+
+  const pending = event?.volunteers?.filter(function(volunteer) {
+      return !volunteer.decisionMade;
+  });
+
+  const approved = event?.volunteers?.filter(function(volunteer) {
+      return volunteer.decisionMade && volunteer.approved;
+  });
+
+  const rejected = event?.volunteers?.filter(function(volunteer) {
+      return volunteer.decisionMade && !volunteer.approved;
+  });
+
   const [userSignedUp, setUserSignedUp] = useState(
     event?.volunteers?.some(v => v.id && v.id === user?.userId) || false,
   );
@@ -147,58 +231,133 @@ function EventDialog(props) {
   }
 
 
-  return (
-    <Dialog
-      classes={{
-        paper: classes.paper,
-      }}
-      open={props.open}
-      onClose={props.handleClose}
-      aria-labelledby="event-dialog-title"
-      aria-describedby="event-dialog-description"
-    >
-      <DialogActions>
-        <IconButton style={{ padding: 0 }} aria-label="close" onClick={props.handleClose}>
-          <CloseIcon />
-        </IconButton>
-      </DialogActions>
-      <DialogTitle id="event-dialog-title">
-        {props.newEvent ?
-          (<React.Fragment>
-            New event for: {getDayMonthDateStr(event.start)}
-          </React.Fragment>) :
-          (<React.Fragment>
-            {event.title}
-            <Tooltip title={tooltipTitle}>
-              <span id="volunteer-sign-up"> {/* Hack to get tooltip to display when button is disabled */}
-                <Button disabled={tooltipTitle.length > 0} onClick={signUpUser}>Sign Up</Button>
-              </span>
-            </Tooltip>
-          </React.Fragment>)
-        }
-      </DialogTitle>
-      <DialogContent>
-        {props.newEvent ?
-          (<React.Fragment>
-            <CreateEvent date={event.start} />
-          </React.Fragment>) :
-          (<React.Fragment>
-            <p>Default Time Slot: {getAMPMTimeRange(event.start, event.end)} on {getDayMonthDateStr(event.start)}</p>
 
-            <p>{event.description}</p>
+     switch (user.userType)
+     {
+         case USER_TYPES.VOLUNTEER: {
+             return (
+             <Dialog
+               classes={{
+                 paper: classes.paper,
+               }}
+               open={props.open}
+               onClose={props.handleClose}
+               aria-labelledby="event-dialog-title"
+               aria-describedby="event-dialog-description"
+             >
+               <DialogActions>
+                 <IconButton style={{ padding: 0 }} aria-label="close" onClick={props.handleClose}>
+                   <CloseIcon />
+                 </IconButton>
+               </DialogActions>
+               <DialogTitle id="event-dialog-title">
+                 {props.newEvent ?
+                   (<React.Fragment>
+                     New event for: {getDayMonthDateStr(event.start)}
+                   </React.Fragment>) :
+                   (<React.Fragment>
+                     {event.title}
+                     <Tooltip title={tooltipTitle}>
+                       <span id="volunteer-sign-up"> {/* Hack to get tooltip to display when button is disabled */}
+                         <Button disabled={tooltipTitle.length > 0} onClick={signUpUser}>Sign Up</Button>
+                       </span>
+                     </Tooltip>
+                   </React.Fragment>)
+                 }
+               </DialogTitle>
+               <DialogContent>
+                 {props.newEvent ?
+                   (<React.Fragment>
+                     <CreateEvent date={event.start} />
+                   </React.Fragment>) :
+                   (<React.Fragment>
+                     <p>Default Time Slot: {getAMPMTimeRange(event.start, event.end)} on {getDayMonthDateStr(event.start)}</p>
 
-            <section>
-              <h6>Volunteers ({event.volunteers?.length || 0})</h6>
-              {userSignedUp && <CustomHours eventStart={event.start} eventEnd={event.end} />}
-              {event.volunteers?.map((volunteer, index) => {
-                return <VolunteerInfo key={index} volunteer={volunteer} />;
-              })}
-            </section>
-          </React.Fragment>)
-        }
-      </DialogContent>
-    </Dialog>
-  );
+                     <p>{event.description}</p>
+
+                     <section>
+                       <h6>Volunteers ({event.volunteers?.length || 0})</h6>
+                       {userSignedUp && <CustomHours eventStart={event.start} eventEnd={event.end} />}
+                       {event.volunteers?.map((volunteer, index) => {
+                         return <VolunteerInfo key={index} volunteer={volunteer} />;
+                       })}
+                     </section>
+                   </React.Fragment>)
+                 }
+               </DialogContent>
+             </Dialog>
+             );
+         }
+         case USER_TYPES.ADMIN: {
+             return (
+                 <Dialog
+                   classes={{
+                     paper: classes.paper,
+                   }}
+                   open={props.open}
+                   onClose={props.handleClose}
+                   aria-labelledby="event-dialog-title"
+                   aria-describedby="event-dialog-description"
+                 >
+                   <DialogActions>
+                     <IconButton style={{ padding: 0 }} aria-label="close" onClick={props.handleClose}>
+                       <CloseIcon />
+                     </IconButton>
+                   </DialogActions>
+                   <DialogTitle id="event-dialog-title">
+                     {props.newEvent ?
+                       (<React.Fragment>
+                         New event for: {getDayMonthDateStr(event.start)}
+                       </React.Fragment>) :
+                       (<React.Fragment>
+                         {event.title}
+                       </React.Fragment>)
+                     }
+                   </DialogTitle>
+                   <DialogContent>
+                     {props.newEvent ?
+                       (<React.Fragment>
+                         <CreateEvent date={event.start} />
+                       </React.Fragment>) :
+                       (<React.Fragment>
+                         <p>Default Time Slot: {getAMPMTimeRange(event.start, event.end)} on {getDayMonthDateStr(event.start)}</p>
+
+                         <p>{event.description}</p>
+
+                         <section>
+                           <h6>Pending ({pending?.length || 0})</h6>
+                           {pending?.map((volunteer, index) => {
+                             return <PendingVolunteerInfo volunteers={event.volunteers}
+                                 setVolunteers={setVolunteers} key={index} volunteer={volunteer}/>;
+                             })}
+                             <br />
+                         </section>
+
+                         <section>
+                           <h6>Approved ({approved?.length || 0})</h6>
+                           {approved?.map((volunteer, index) => {
+                             return <ApprovedVolunteerInfo volunteers={event.volunteers}
+                                 setVolunteers={setVolunteers} key={index} volunteer={volunteer}/>;
+                           })}
+                           <br />
+                         </section>
+
+                         <section>
+                           <h6>Rejected ({rejected?.length || 0})</h6>
+                           {rejected?.map((volunteer, index) => {
+                             return <RejectedVolunteerInfo volunteers={event.volunteers}
+                                 setVolunteers={setVolunteers} key={index} volunteer={volunteer}/>;
+                           })}
+                           <br />
+                         </section>
+                       </React.Fragment>)
+                     }
+                   </DialogContent>
+                 </Dialog>
+             );
+         }
+     }
+
 }
 
 export default EventDialog;
