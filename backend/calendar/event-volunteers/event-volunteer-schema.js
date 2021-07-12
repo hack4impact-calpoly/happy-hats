f11d929866f612d5caccb49fed0493d2f7f24640
bglossner <benglossner@gmail.com>
2021-07-12 13:51:36 -0700

    hopefully fix deployed error

--- backend/calendar/event-volunteers/event-volunteer-schema.js
+++ backend/calendar/event-volunteers/event-volunteer-schema.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
-const volunteerSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    id: { type: mongoose.Types.ObjectId, required: true }
-}, { _id: false });
-
 const eventVolunteerSchema = new mongoose.Schema({
     start: { type: Date, required: false },
     end: { type: Date, required: false },
-    volunteer: { type: volunteerSchema, required: true },
+    volunteer: {
+        type: {
+            firstName: { type: String, required: true },
+            lastName: { type: String, required: true },
+            email: { type: String, required: true },
+            id: { type: mongoose.Types.ObjectId, required: true }
+        },
+        required: true,
+    },
     usingDefaultTimes: { type: Boolean, required: true },
     approved: { type: Boolean, required: false },
     decisionMade: { type: Boolean, required: false, default: false },
